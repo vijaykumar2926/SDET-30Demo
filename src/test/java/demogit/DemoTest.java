5e073f835e96cf33a2dcbf64d3fb9746227f9147
vijay <vijay@LAPTOP-0SCJ6J7A>
2022-03-14 12:58:36 +0530

    commit

---
+++ src/test/java/demogit/DemoTest.java
@@ -0,0 +1,26 @@
+package demogit;
+
+import org.testng.annotations.Test;
+
+public class DemoTest 
+{
+	@Test
+	public void demo()
+	{
+		System.out.println("Printing from method2");
+		
+	}
+	
+	@Test
+	public void Demo2()
+	{
+		System.out.println("Printing from method2");
+	}
+	
+	
+	@Test
+	public void Demo3()
+	{
+		System.out.println("Printing from method3");
+	}
+}
